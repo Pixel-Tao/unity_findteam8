aee98baae4ea07a1ce2f7fdfb4d73579945984b0
이민석 <leem980425@gmail.com>
2024-09-09 19:47:55 +0900

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,31 +7,12 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SoundManager.inst.BSound(AudioType.BGM);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-            SoundManager.inst.ESound(AudioType.Ball01);
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-            SoundManager.inst.ESound(AudioType.Ball02);
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-            SoundManager.inst.ESound(AudioType.Ball03);
-
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-            SoundManager.inst.ESound(AudioType.Click);
-
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-            SoundManager.inst.ESound(AudioType.Win);
-
-        if (Input.GetKeyDown(KeyCode.Alpha6))
-            SoundManager.inst.ESound(AudioType.Defeat);
-
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-            SoundManager.inst.BSound(AudioType.None);
+ 
     }
 }
