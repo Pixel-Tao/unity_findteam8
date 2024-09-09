b22d81842fc365ccd87d03896ce0379e08c03e91
yoonday <yoon2dev@gmail.com>
2024-09-09 20:21:26 +0900

    [Add] BackImage
    공 이미지 추가

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -12,8 +12,11 @@ public class Board : MonoBehaviour
 
     void Start()
     {
-        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-        arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
+        int[] frontArr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        frontArr = frontArr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
+
+        int[] backArr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        backArr = backArr.OrderBy(x => Random.Range(0f, 15f)).ToArray();
 
 
         for (int i = 0; i < 16; i++)
@@ -24,8 +27,11 @@ public class Board : MonoBehaviour
             float y = (i / 4) * 1.4f - 2.0f;
 
             go.transform.position = new Vector2(x, y);
-            
-            go.GetComponent<Card>().Setting(arr[i]);
+
+            int frontImage = frontArr[i];
+            int backImage = backArr[i];
+
+            go.GetComponent<Card>().Setting(frontImage, backImage);
         }
 
 
--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
     public int idx = 0;
+    public int idx2 = 0;
 
     public GameObject front; // 카드 앞면
     public GameObject back;  // 카드 뒷면
@@ -29,11 +30,26 @@ public class Card : MonoBehaviour
 
     }
 
-    public void Setting(int number)
+    public void Setting(int frontNumber, int backNumber)
 
     {
-        idx = number;
-        frontImage.sprite = Resources.Load<Sprite>($"rtan{idx}"); 
+        idx = frontNumber;
+        idx2 = backNumber;
+        frontImage.sprite = Resources.Load<Sprite>($"rtan{frontNumber}");
+        backImage.sprite = Resources.Load<Sprite>($"backColor{backNumber}");
+
+        Debug.Log($"Setting frontNumber: {frontNumber}, backNumber: {backNumber}");
+
+        frontImage.sprite = Resources.Load<Sprite>($"rtan{frontNumber}");
+        backImage.sprite = Resources.Load<Sprite>($"backColor{backNumber}");
+
+        // 이미지가 제대로 로드되지 않으면 로그를 남김
+        if (frontImage.sprite == null)
+            Debug.LogError($"Failed to load front sprite for rtan{frontNumber}");
+
+        if (backImage.sprite == null)
+            Debug.LogError($"Failed to load back sprite for backColor{backNumber}");
+
 
     }
 
